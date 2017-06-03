9e0352c30e23dd62f22bc9bbe807f88aac459609
ngas-wakefield <ngaswakefield@gmail.com>
2017-06-03 14:02:00 +1200

    added 1,2,3,4 functions

--- classes.js
+++ classes.js
@@ -5,9 +5,16 @@ document.addEventListener('DOMContentLoaded', start)
 
 function start () {
   // The first example is done for you. Uncomment the line below and reload the browser.
-  // one()
+  one()
     
   // Your turn! Create a new function called `two`, then call it from here.
+    
+    two()
+    
+    
+    three()
+    
+    makeVisible()
 }
 
 function one () {
@@ -20,6 +27,31 @@ function one () {
 
 // CREATE FUNCTION two HERE
 
+function two () {
+  // First, we have to find the element:
+  var two = document.getElementById('two')
+
+  // Next, we apply a new CSS class to it:
+  two.classList.add('green')
+}
+
+
 // CREATE FUNCTION three HERE
 
+
+function three () {
+  // First, we have to find the element:
+  var three = document.getElementById('three')
+
+  // Next, we apply a new CSS class to it:
+  three.classList.add('black')
+}
+
+
 // CREATE FUNCTION makeVisible HERE
+
+
+function makeVisible () {
+  var makeVisible = document.getElementsByClassName('invisible')[0]
+  makeVisible.classList.add('visible')
+}
